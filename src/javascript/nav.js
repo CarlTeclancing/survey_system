51d773a216860b3e9e2774d3b6833b0813278bd8
YuvCarl <carldrake969@gmail.com>
2023-03-05 10:27:01 +0100

    changes made for first commit

---
+++ src/javascript/nav.js
@@ -0,0 +1,12 @@
+var TurgleNav = document.getElementById("turgle-nav");
+
+TurgleNav.addEventListener("click", function(){
+    var navBar = document.getElementById("navbarNav");
+    navBar.style.display = "block";
+    navBar.style.display = "flex"
+    navBar.style.flexDirection = "row"
+    navBar.style.display = "flex"
+    navBar.style.zIndex = "2";
+
+
+})
